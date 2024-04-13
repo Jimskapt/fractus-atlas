ad9f28e81aca6c120c95ae1bc772cc2734bb65f8
Jimskapt <rami.programmer@gmail.com>
2024-04-13 19:30:01 +0200

    🚚 Move wasm save code in rust submodules.

--- src-wasm/src/input_controller.rs
+++ src-wasm/src/input_controller.rs
@@ -66,3 +66,76 @@ pub fn update_input_filters(id: usize, i: usize) {
 		crate::input_view::generate_input_filter(&document, &filters_container, id, i + 1, None);
 	}
 }
+
+pub fn save() -> (Vec<common::InputFolder>, Vec<common::SaveMessage>) {
+	let mut input_folders = vec![];
+	let mut messages = vec![];
+
+	let window = web_sys::window().unwrap();
+	let document = window.document().unwrap();
+
+	let mut i = 0;
+	while let Some(field) = document
+		.query_selector(&format!("#input_folder_path_{i}"))
+		.unwrap()
+	{
+		let path_value = field
+			.dyn_into::<web_sys::HtmlInputElement>()
+			.unwrap()
+			.value();
+
+		let name_value = document
+			.query_selector(&format!("#input_folder_name_{i}"))
+			.unwrap()
+			.unwrap()
+			.dyn_into::<web_sys::HtmlInputElement>()
+			.unwrap()
+			.value();
+
+		if !path_value.trim().is_empty() {
+			let mut filters = vec![];
+			let mut i_filter = 1;
+			while let Some(filter_field) = document
+				.query_selector(&format!("#input_folder_filter_value_{i}_{i_filter}"))
+				.unwrap()
+			{
+				let value = filter_field
+					.dyn_into::<web_sys::HtmlInputElement>()
+					.unwrap()
+					.value();
+
+				if !value.trim().is_empty() {
+					let filter_type = document
+						.query_selector(&format!("#input_folder_filter_type_{i}_{i_filter}"))
+						.unwrap()
+						.unwrap()
+						.dyn_into::<web_sys::HtmlSelectElement>()
+						.unwrap()
+						.value();
+
+					if filter_type == "extension" {
+						filters.push(common::FileFilter::Extension(value.trim().to_lowercase()));
+					} else {
+						todo!()
+					}
+				}
+
+				i_filter += 1;
+			}
+
+			input_folders.push(common::InputFolder {
+				name: if name_value.trim().is_empty() {
+					None
+				} else {
+					Some(String::from(name_value.trim()))
+				},
+				path: std::path::PathBuf::from(path_value),
+				filters,
+			});
+		}
+
+		i += 1;
+	}
+
+	(input_folders, messages)
+}
--- src-wasm/src/output_controller.rs
+++ src-wasm/src/output_controller.rs
@@ -61,3 +61,72 @@ pub fn update_output_shortcuts(id: usize, i: usize) {
 		);
 	}
 }
+
+pub fn save() -> (Vec<common::OutputFolder>, Vec<common::SaveMessage>) {
+	let mut output_folders = vec![];
+	let mut messages = vec![];
+
+	let window = web_sys::window().unwrap();
+	let document = window.document().unwrap();
+
+	let mut i = 0;
+	while let Some(field) = document
+		.query_selector(&format!("#output_folder_path_{i}"))
+		.unwrap()
+	{
+		let path_value = field
+			.dyn_into::<web_sys::HtmlInputElement>()
+			.unwrap()
+			.value();
+
+		let name_value = document
+			.query_selector(&format!("#output_folder_name_{i}"))
+			.unwrap()
+			.unwrap()
+			.dyn_into::<web_sys::HtmlInputElement>()
+			.unwrap()
+			.value();
+
+		if !path_value.trim().is_empty() {
+			let mut shortcuts = vec![];
+			let mut i_shortcut = 1;
+			while let Some(shortcut_field) = document
+				.query_selector(&format!("#output_folder_shortcut_value_{i}_{i_shortcut}"))
+				.unwrap()
+			{
+				let value = shortcut_field
+					.dyn_into::<web_sys::HtmlInputElement>()
+					.unwrap()
+					.value();
+
+				if !value.trim().is_empty() {
+					let shortcut_type = document
+						.query_selector(&format!("#output_folder_shortcut_type_{i}_{i_shortcut}"))
+						.unwrap()
+						.unwrap()
+						.dyn_into::<web_sys::HtmlSelectElement>()
+						.unwrap()
+						.value();
+
+					if shortcut_type == "key" {
+						shortcuts.push(common::Shortcut::Key(value.trim().to_lowercase()));
+					} else {
+						todo!()
+					}
+				}
+
+				i_shortcut += 1;
+			}
+
+			output_folders.push(common::OutputFolder {
+				name: String::from(name_value.trim()),
+				path: std::path::PathBuf::from(path_value),
+				shortcuts_or: shortcuts,
+			});
+		}
+
+		i += 1;
+	}
+
+	(output_folders, messages)
+}
